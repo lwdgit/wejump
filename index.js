c357e69b7a612d0cb4c6dffefe71474fdebdf52e
lwdggm <lwdggm@gmail.com>
2018-01-02 00:46:57 +0800

    fix: 增加正系数

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ async function start() {
   assert(devices && devices.length, '没有检测到安卓设备')
   var deviceId = devices[0].id
   while (1) {
-    await sleep((Math.random() * 200) + 3000)
+    await sleep((Math.random() * 200) + 4000)
     var stream = await client.screencap(deviceId)
     var im = await parseStream(stream)
     var [piece_x, piece_y, board_x, board_y] = find_piece_and_board(im)
-    jump(deviceId, sqrt(abs(board_x - piece_x) ** 2 + abs(board_y - piece_y) ** 2))
+    if (piece_x === 0) break
+    await jump(deviceId, sqrt(abs(board_x - piece_x) ** 2 + abs(board_y - piece_y) ** 2))
+    im.pack('out.png')
   }
 }
 start()
@@ -31,12 +33,14 @@ function parseStream (stream) {
       filterType: 4
     }))
     .on('parsed', function () {
-      // this.pack().pipe(fs.createWriteStream('out.png'));
       var self = this
       var im = {
         getpixel(x, y) {
           var idx = (self.width * y + x) << 2
           return [self.data[idx], self.data[idx + 1], self.data[idx + 2]]
+        },
+        pack (path) {
+          return self.pack().pipe(fs.createWriteStream(path))
         }
       }
       Object.defineProperties(im, {
@@ -61,17 +65,11 @@ function int (str) {
   return parseInt(str, 10)
 }
 
-var old = 1
 function jump(deviceId, distance) {
-  var press_time = distance * 1.450
-  print(new Date() + ' press_time: ' + str(press_time))
-  press_time = int(press_time)
-  if (press_time > 300 || old === press_time) {
-    old = 1
-    client.shell(deviceId, 'input swipe 320 410 320 410 ' + str(press_time))
-  } else {
-    old = press_time
-  }
+  var press_time = distance * 1.40
+  var offset = press_time < 220 ? 0 : 40 / press_time
+  print(new Date() + ' press_time: ' + str(press_time) + ' ' + offset)
+  return client.shell(deviceId, 'input swipe 320 410 320 410 ' + Math.round(press_time * (1 + offset)))
 }
 
 function all(arr) {
@@ -80,6 +78,10 @@ function all(arr) {
 
 function max(...args) {
   return Math.max(...args)
+}
+
+function min(...args) {
+  return Math.min(...args)
 }
 
 function abs(...args) {
@@ -97,65 +99,72 @@ function sqrt (...args) {
 function find_piece_and_board(im) {
   var [w, h] = im.size
 
-  var piece_x_sum = 0
-  var piece_x_c = 0
+  var piece_x_min = w
+  var piece_x_max = 0
   var piece_y_max = 0
   var board_x = 0
   var board_y = 0
-
-  for (var i of range(h)) {
-    for (var j of range(w)) {
+  var board_x_min = w
+  var board_x_max = 0
+  var x1 = 0
+  var x2 = 0
+  
+  var ratio = 1.8
+  
+  for (var i = 300; i <= h; i++) {
+    for (var j = 0; j <= w; j++) {
       var pixel = im.getpixel(j, i)
-      // 根据棋子的最低行的颜色判断，找最后一行那些点的平均值
-      // print(pixel[0])
-      if ((pixel[0] > 50 && pixel[0] < 60) && (pixel[1] > 53 && pixel[1] < 63) && (pixel[2] > 95 && pixel[2] < 110)) {
-        piece_x_sum += j
-        piece_x_c += 1
-        piece_y_max = max(i, piece_y_max)
+      if ((pixel[0] > 50 && pixel[0] < 60) && (pixel[1] > 50 && pixel[1] < 63) && (pixel[2] > 95 && pixel[2] < 110)) { // 棋子颜色
+        // 更改 x 平均值算法
+        piece_x_min = min(j, piece_x_min)
+        piece_x_max = max(j, piece_x_max)
+        piece_y_max = i
       }
     }
   }
-  if (!all([piece_x_sum, piece_x_c])) {
+
+  if (piece_x_max < piece_x_min) {
     return [0, 0, 0, 0]
   }
-  var piece_x = piece_x_sum / piece_x_c
-  // TODO: 大小根据截图的 size 来计算
-  var piece_y = piece_y_max - 20  // 上移棋子底盘高度的一半
+  var piece_x = (piece_x_min + piece_x_max) / 2
+  var piece_y = piece_y_max - (piece_x_max - piece_x_min) / ratio  // 计算底盘
 
-  for (var i of range(h)) {
-    if (i < 300) {
-      continue
-    }
-    var last_pixel = im.getpixel(0, i)
-    if (board_x || board_y) {
-      break
-    }
-    board_x_sum = 0
-    board_x_c = 0
-
-
-    for (var j of range(w)) {
-      var pixel = im.getpixel(j, i)
-      // 修掉脑袋比下一个小格子还高的情况的 bug
-      if (abs(j - piece_x) < 70) {
+  var bg_pixel = im.getpixel(300, 0)
+  for (var i = 300; i < piece_y; i++) {
+    board_x_min = w
+    board_x_max = 0
+    for (var j = 0; j <= w; j++) {
+      if (abs(j - piece_x_min) < 40 || abs(j - piece_x_max) < 40) { // 处理header
         continue
       }
-
-      // 修掉圆顶的时候一条线导致的小 bug
-      if (abs(pixel[0] - last_pixel[0]) + abs(pixel[1] - last_pixel[1]) + abs(pixel[2] - last_pixel[2]) > 10) {
-        board_x_sum += j
-        board_x_c += 1
+      var pixel = im.getpixel(j, i)
+      if (abs(pixel[0] - bg_pixel[0]) + abs(pixel[1] - bg_pixel[1]) + abs(pixel[2] - bg_pixel[2]) < 30) { // 处理渐变背景色
+        bg_pixel = pixel
+      } else { // 如果还没找到水平中点，则
+        board_x_max = max(board_x_max, j)
+        board_x_min = min(board_x_min, j)
       }
-      if (board_x_sum) {
-        board_x = board_x_sum / board_x_c
-      }
+    }
+    if (board_x_max >= board_x_min) {
+      board_y_min = i
+      break
     }
   }
 
-  board_y = piece_y + abs(board_x - piece_x) * abs(1122 - 831) / abs(813 - 310)   // 按实际的角度来算，找到接近下一个 board 中心的坐标
-
-  if (!all([board_x, board_y])) {
+  if (board_x_max < board_x_min) {
     return [0, 0, 0, 0]
   }
+
+  // if (board_x_max - board_x_min > 5) {
+  //   console.log('这是一个椭圆')
+  // } else {
+  //   console.log('这是一个方形')
+  // }
+  board_x = (board_x_max + board_x_min) / 2
+
+  if (!board_y_min) {
+    return [0, 0, 0, 0]
+  }
+  board_y = piece_y - abs(board_x - piece_x) / ratio
   return [piece_x, piece_y, board_x, board_y]
 }
